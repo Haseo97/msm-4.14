b250c86c3136315724cd715718e3acce3a964e70
Al Viro <viro@zeniv.linux.org.uk>
2018-07-11 14:19:04 -0400

    alloc_file(): switch to passing O_... flags instead of FMODE_... mode
    ... so that it could set both ->f_flags and ->f_mode, without callers
    having to set ->f_flags manually.
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Yousef Algadri <yusufgadrie@gmail.com>
    Change-Id: I22053fcd26beba098cee85d92b0a09cd3e79a709

--- mm/shmem.c
+++ mm/shmem.c
@@ -4298,8 +4298,7 @@ static struct file *__shmem_file_setup(const char *name, loff_t size,
 	if (IS_ERR(res))
 		goto put_path;
 
-	res = alloc_file(&path, FMODE_WRITE | FMODE_READ,
-		  &shmem_file_operations);
+	res = alloc_file(&path, O_RDWR, &shmem_file_operations);
 	if (IS_ERR(res))
 		goto put_path;
 
