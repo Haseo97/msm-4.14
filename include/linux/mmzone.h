48f13f149d42da9887b1312f0481fc2192348f5f
Minchan Kim <minchan@google.com>
2019-03-11 15:19:08 +0900

    kgsl: add GPU driver memory consumption in vmstat
    There was hole to calculate memory consumption due to lacking
    of GPU driver memory consumption.
    
    This patch adds GPU driver memory consumption to vmstat.
    It would be handy when we calculate memory.
    
    Bug: 128254966
    Bug: 130198686
    Test: boot and check /proc/vmstat has nr_gpu_heap
    Change-Id: Idb322f80dbfde785720f01b3ad9954eb4feb7326
    Signed-off-by: Minchan Kim <minchan@google.com>
    Signed-off-by: Kyle Lin <kylelin@google.com>
    Signed-off-by: Yousef Algadri <yusufgadrie@gmail.com>

--- include/linux/mmzone.h
+++ include/linux/mmzone.h
@@ -188,6 +188,7 @@ enum node_stat_item {
 	NR_WRITTEN,		/* page writings since bootup */
 	NR_INDIRECTLY_RECLAIMABLE_BYTES, /* measured in bytes */
 	NR_UNRECLAIMABLE_PAGES,
+	NR_GPU_HEAP,
 	NR_VM_NODE_STAT_ITEMS
 };
 
--- mm/vmstat.c
+++ mm/vmstat.c
@@ -1096,7 +1096,7 @@ const char * const vmstat_text[] = {
 	"nr_written",
 	"nr_indirectly_reclaimable",
 	"nr_unreclaimable_pages",
-
+	"nr_gpu_heap",
 	/* enum writeback_stat_item counters */
 	"nr_dirty_threshold",
 	"nr_dirty_background_threshold",
