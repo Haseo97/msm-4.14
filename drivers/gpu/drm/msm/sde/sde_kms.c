f4942cc94d2047ad0626c220e3324201403ff6d8
Danny Lin <danny@kdrag0n.dev>
2019-05-10 21:36:06 -0700

    drm/msm/dp: Modularize DisplayPort support
    Don't force DisplayPort support to always be compiled into the driver.
    More often than not, it is unused and sometimes cannot be used due to
    platform limitations.
    
    msm_drm driver size reduction after this change: 21%
        Delta: 511 KiB (2385 KiB -> 1874 KiB)
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: Yousef Algadri <yusufgadrie@gmail.com>

--- drivers/gpu/drm/msm/sde/sde_kms.c
+++ drivers/gpu/drm/msm/sde/sde_kms.c
@@ -1309,6 +1309,7 @@ static int _sde_kms_get_displays(struct sde_kms *sde_kms)
 #endif
 
 	/* dp */
+#ifdef CONFIG_DRM_MSM_DP
 	sde_kms->dp_displays = NULL;
 	sde_kms->dp_display_count = dp_display_get_num_of_displays();
 	if (sde_kms->dp_display_count) {
@@ -1324,13 +1325,16 @@ static int _sde_kms_get_displays(struct sde_kms *sde_kms)
 
 		sde_kms->dp_stream_count = dp_display_get_num_of_streams();
 	}
+#endif
 	return 0;
 
+#ifdef CONFIG_DRM_MSM_DP
 exit_deinit_dp:
 	kfree(sde_kms->dp_displays);
 	sde_kms->dp_stream_count = 0;
 	sde_kms->dp_display_count = 0;
 	sde_kms->dp_displays = NULL;
+#endif
 
 #ifdef CONFIG_DRM_SDE_WB
 exit_deinit_wb:
@@ -1419,6 +1423,7 @@ static int _sde_kms_setup_displays(struct drm_device *dev,
 		.get_panel_vfp = NULL,
 	};
 #endif
+#ifdef CONFIG_DRM_MSM_DP
 	static const struct sde_connector_ops dp_ops = {
 		.set_info_blob = dp_connnector_set_info_blob,
 		.post_init  = dp_connector_post_init,
@@ -1435,6 +1440,7 @@ static int _sde_kms_setup_displays(struct drm_device *dev,
 		.get_panel_vfp = NULL,
 		.update_pps = dp_connector_update_pps,
 	};
+#endif
 	struct msm_display_info info;
 	struct drm_encoder *encoder;
 	void *display, *connector;
@@ -1446,10 +1452,12 @@ static int _sde_kms_setup_displays(struct drm_device *dev,
 		return -EINVAL;
 	}
 
-	max_encoders = sde_kms->dsi_display_count + sde_kms->dp_display_count +
-				sde_kms->dp_stream_count;
+	max_encoders = sde_kms->dsi_display_count;
 #ifdef CONFIG_DRM_SDE_WB
 	max_encoders += sde_kms->wb_display_count;
+#endif
+#ifdef CONFIG_DRM_MSM_DP
+	max_encoders += sde_kms->dp_display_count + sde_kms->dp_stream_count;
 #endif
 	if (max_encoders > ARRAY_SIZE(priv->encoders)) {
 		max_encoders = ARRAY_SIZE(priv->encoders);
@@ -1554,6 +1562,7 @@ static int _sde_kms_setup_displays(struct drm_device *dev,
 		}
 	}
 #endif
+#ifdef CONFIG_DRM_MSM_DP
 	/* dp */
 	for (i = 0; i < sde_kms->dp_display_count &&
 			priv->num_encoders < max_encoders; ++i) {
@@ -1619,6 +1628,7 @@ static int _sde_kms_setup_displays(struct drm_device *dev,
 			priv->encoders[priv->num_encoders++] = encoder;
 		}
 	}
+#endif
 
 	return 0;
 }
